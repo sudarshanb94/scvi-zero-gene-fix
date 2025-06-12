5f0778378928dcd81dd93d9b82ab9a357e52b4ab
Mohsen <naghipourfar@berkeley.edu>
2025-06-12 15:04:49 -0700

    minor issue in train.sh fixed

--- baselines/scripts/train.sh
+++ baselines/scripts/train.sh
@@ -21,10 +21,30 @@ if [ $DATASET_NAME == "replogle" ]; then
 
     OUTPUT_DIR="${OUTPUT_DIR_BASE}/${MODEL_NAME}_replogle/fold${FOLD_ID}/"
     WANDB_TAGS="[${MODEL_NAME},replogle,fold${FOLD_ID}]"
+    TRAINING_NAME=${MODEL_NAME}
+
+    if [ $MODEL_NAME == "scgpt" ]; then
+        MODEL_NAME="scgpt-genetic"
+        TRAINING_NAME="scgpt"
+    fi
+
+    BATCH_COL="gem_group"
+    PERT_COL="gene"
+    CELL_TYPE_KEY="cell_type"
+    CONTROL_PERT="non-targeting"
 elif [ $DATASET_NAME == "tahoe" ]; then
     DATA_TOML_PATH="/large_storage/ctc/userspace/aadduri/datasets/tahoe_5_holdout/generalization.toml"
     OUTPUT_DIR="${OUTPUT_DIR_BASE}/${MODEL_NAME}_tahoe/tahoe_generalization/"
     WANDB_TAGS="[${MODEL_NAME},tahoe,generalization]"
+    if [ $MODEL_NAME == "scgpt" ]; then
+        MODEL_NAME="scgpt-chemical"
+        TRAINING_NAME="scgpt"
+    fi
+
+    BATCH_COL="sample"
+    PERT_COL="drugname_drugconc"
+    CELL_TYPE_KEY="cell_name"
+    CONTROL_PERT="DMSO_TF"
 fi
 
 echo "Training $MODEL_NAME on $DATASET_NAME fold $FOLD_ID"
@@ -39,10 +59,10 @@ uv run python -m state_sets_reproduce.train \
     data.kwargs.basal_mapping_strategy=random \
     data.kwargs.output_space=gene \
     data.kwargs.num_workers=24 \
-    data.kwargs.batch_col=gem_group \
-    data.kwargs.pert_col=gene \
-    data.kwargs.cell_type_key=cell_type \
-    data.kwargs.control_pert=non-targeting \
+    data.kwargs.batch_col=${BATCH_COL} \
+    data.kwargs.pert_col=${PERT_COL} \
+    data.kwargs.cell_type_key=${CELL_TYPE_KEY} \
+    data.kwargs.control_pert=${CONTROL_PERT} \
     training.max_steps=50000 \
     training.val_freq=1000 \
     training.test_freq=2000 \
@@ -50,6 +70,6 @@ uv run python -m state_sets_reproduce.train \
     wandb=mohsen \
     wandb.tags="${WANDB_TAGS}" \
     model=${MODEL_NAME} \
-    training=${MODEL_NAME} \
+    training=${TRAINING_NAME} \
     output_dir="${OUTPUT_DIR}" \
     name="fold${FOLD_ID}"
